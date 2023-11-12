333bbb6eefe89c63187505d7d37dde3a61800728
minji12344 <alswl12344@naver.com>
2023-11-12 22:57:58 +0900

    Feat : 라우터 추가

--- routes/community.js
+++ routes/community.js
@@ -68,9 +68,54 @@ router.post('/delete', function (req, res) {
 
 // 개발자 라운지 목록
 router.get('/loungelist.json', function(req, res){
-    const sql='SELECT p.*, u.nickname, u.profile_image, c.comment_id, date_format(p.created_at, "%Y.%m.%d %T") fmtdate FROM posts p inner join users u on p.user_id = u.user_id  inner join comments c on p.user_id = c.user_id where p.menu = 3;'
+    const sql='SELECT p.*, u.nickname, date_format(p.created_at, "%Y.%m.%d %T") fmtdate FROM posts p,users u where p.user_id = u.user_id and p.menu = 3;'
     db.get().query(sql,function(err, rows){
         res.send(rows);
     })
 });
+
+// 개발자 라운지 정보
+router.get('/lounge/:post_id', function(req, res){// localhost:5000/community/lounge/50
+    const post_id=req.params.post_id;
+    const sql='SELECT p.*, u.nickname, date_format(p.created_at, "%Y.%m.%d %T") fmtdate FROM posts p,users u where p.user_id = u.user_id and p.menu = 3 and post_id=?;'
+    db.get().query(sql, [post_id],function(err, rows){
+        res.send(rows);
+        console.log(rows)
+    })
+});
+
+// 프로젝트 & 스터디 목록
+router.get('/applystudy&project.json', function(req, res){
+    const sql='SELECT p.*, u.nickname, date_format(p.created_at, "%Y.%m.%d %T") fmtdate FROM posts p,users u where p.user_id = u.user_id and p.menu = 4;'
+    db.get().query(sql,function(err, rows){
+        res.send(rows);
+    })
+});
+// 프로젝트 & 스터디 정보
+router.get('/applystudy&project/:post_id', function(req, res){// localhost:5000/community/applystudy&project/51
+    const post_id=req.params.post_id;
+    const sql='SELECT p.*, u.nickname, date_format(p.created_at, "%Y.%m.%d %T") fmtdate FROM posts p,users u where p.user_id = u.user_id and p.menu = 4 and post_id=?;'
+    db.get().query(sql, [post_id],function(err, rows){
+        res.send(rows);
+        console.log(rows)
+    })
+});
+
+// Q & A 목록
+router.get('/q&a.json', function(req, res){
+    const sql='SELECT p.*, u.nickname, date_format(p.created_at, "%Y.%m.%d %T") fmtdate FROM posts p,users u where p.user_id = u.user_id and p.menu = 5;'
+    db.get().query(sql,function(err, rows){
+        res.send(rows);
+    })
+});
+// Q & A 정보
+router.get('/q&a/:post_id', function(req, res){// localhost:5000/community/q&a/52
+    const post_id=req.params.post_id;
+    const sql='SELECT p.*, u.nickname, date_format(p.created_at, "%Y.%m.%d %T") fmtdate FROM posts p,users u where p.user_id = u.user_id and p.menu = 5 and post_id=?;'
+    db.get().query(sql, [post_id],function(err, rows){
+        res.send(rows);
+        console.log(rows)
+    })
+});
+
 module.exports = router;
